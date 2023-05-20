0fc017ead1a83440dc0498005dcbe986fabe080a
Ivan Samarskyi <samariva@fit.cvut.cz>
2023-05-20 11:48:40 +0200

    New implementation for filter and image classes

--- src/cimage.h
+++ src/cimage.h
@@ -2,86 +2,132 @@
 #define IMAGE
 
 #include <string>
-
-/**
- * A structure which will store all information about filters to be applied
-*/
-struct CFilters {
-    //Generate a default set of filters (pre-defined)
-    CFilters ();
-
-    //Each filter should be able to be set manually
-    void setGradient ( std::string & gradient );
-
-    void setNegative ( bool negative );
-
-    void setSize ( double size );
-
-    void setBrightness ( double brightness );
-    
-    void setRotation ( double rotation );
-
-    void setConvolution ( bool convolution );
-
-    //Member variables for storing settings of each filter
-    std::string m_gradient;
-    bool m_negative;
-    double m_size;
-    double m_brightness;
-    int m_rotation;
-    bool m_convolution;
-};
+#include <vector>
 
 /**
  * An base class which represents an image
  * provided by a user to be proceed in an ASCII art
 */
 class CImage {
+
+private:
+private:
+
+    class CFilter {
     public:
-        //implicit constructor with default(programmed) parameters
-        CImage( const std::string & = "" );
+        //An abstract base class won't have a constructor
+        CFilter() = delete;
 
-        // void setGradient ( std::string & gradient );
+        virtual ~CFilter() = default;
 
-        // void setNegative ( bool negative );
+        //Applies a filter to an image
+        virtual void apply() = 0;
+    };
 
-        // void setSize ( double size );
+    class CGradient : public CFilter {
+    public:
+        CGradient ( const char * sequence = "" );
 
-        // void setBrightness ( double brightness );
-        
-        // void setRotation ( double rotation );
-
-        // void setConvolution ( bool convolution );
-
-        /**
-         * A method saves filters which will be applied to that particular image
-         * @param[in] filters A const reference to filters for that image
-        */
-        void saveFilters( CFilters & filters ) noexcept;
-
-        // void grayscale();
-
-        // void convertToASCII();
-
-        /**
-         * A method will save a ASCII art to the file filenameASCII.txt
-        */
-        void saveToFile( std::string & filename ) const;
-        
-        /**
-         * A method will print out an ASCII art 
-        */
-        // void showImage() const noexcept;
+        void apply() override;
 
     private:
+        std::string m_gradient;
+    };
+
+    class CBrightness : public CFilter {
+    public:
+        CBrightness ( int brightness );
+
+        void apply() override;
+
+    private:
+        int m_brightness;
+    };
+
+    class CContrast : public CFilter {
+    public:
+        CContrast ( int contrast );
+
+        void apply() override;
+
+    private:
+        int m_contrast;
+    };
+
+    class CNegative : public CFilter {
+    public:
+        CNegative ( bool is_enabled );
+
+        void apply() override;
+
+    private:
+        bool m_is_enabled;
+    };
+
+    class CScale : public CFilter {
+    public:
+        CScale ( int scale );
+
+        void apply() override;
+
+    private:
+        int m_scale;
+    };
+
+    class CConvolution : public CFilter {
+    public:
+        CConvolution ( bool is_enabled );
+
+        void apply() override;
+
+    private:
+        bool m_is_enabled;
+    };
+
+public:
+    //implicit constructor with default(programmed) parameters
+    CImage();
+
+    //Adds a filter to an image
+    void addFilter( CFilter & filter );
+
+    /**
+     * A method applies filters to an image
+     * @param[in] filters A const reference to filters for that image
+    */
+    void applyFilters();
+
+    /**
+     * A method grayscale the image
+    */
+    void grayscale();
+
+    /**
+    * A method converts an image to ASCII
+    */
+    void convert();
+
+    /**
+     * A method will save a ASCII art to the txt file
+    */
+    void saveToFile() const;
+
+    /**
+     * A method will print out an ASCII art
+    */
+        void print() const noexcept;
+private:
+
         //A set of filters to be applied
-        CFilters m_filters;
+        std::vector<CFilter> m_filters;
         //raw pixels data
         char * m_image_data;
         //ascii converted data
         char * m_ascii_data;
         //name of the image file
         std::string m_filename;
+        //name of the file type
+        std::string m_filetype;
 };
 
 #endif //IMAGE
