41401a86c99b3a168e62dd1119f000318474fa4d
Ivan Samarskyi <samariva@fit.cvut.cz>
2023-05-20 10:37:57 +0200

    Changed the main functions, added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include "asciiConverter.h"
+#include "convertToAscii.h"
+#include "presentResults.h"
 #include "readConfig.h"
-
-void runPresentation(CStorage &images)
-{
-
-}
 
 int main(int argc, char *argv[])
 {
@@ -15,15 +11,30 @@ int main(int argc, char *argv[])
 
     try
     {
-        // Process the config.txt file
+        /**
+         * The function reads the configuration file which was provided to the application
+         * @param[in] images A container where images will be stored
+         * @exception runtime_error May throw a runtime error if the config file doesn't match the example or files
+         * which were provided to the program don't exist/are broken
+         */
         readConfig(images);
 
-        // Make a conversion for all files provided, show statistcs
-        //  asciiConverter( images );
+        /**
+         * The function converts images to ASCII-arts, saves results in %name of the image%.txt files
+         * @param[in] images A container of images to be converted
+         * @exception runtime_error May throw a runtime error if files which were provided to the program don't
+         * exist/are broken
+         */
+        convertToAscii( images );
 
-        // Ask the user if he wants a presentation, if so, show it
-        //  runPresentation( images );
+        /**
+         * The function communicates with a user and presents the results of conversion
+         * @param[in] images A container of images to be presented
+         * @exception runtime_error May throw a runtime error if files with converted images don't exist/ are broken
+         */
+        presentResults( images );
     }
+    //Catches all possible errors the program  may throw
     catch (std::exception & e )
     {
         std::cout << "ERROR: " << e.what() << std::endl;
